d9c9f8d2b9a04d41a9babe18908994d1930a7c89
Crane <215379951@qq.com>
2017-10-29 19:57:45 +0800

    changed view-all-plugins.sh

--- view-all-plugins.sh
+++ view-all-plugins.sh
@@ -7,5 +7,13 @@
 #
 
 cd  .vim/vim-scripts/
-grep  -E  "Bundle\s+[\"'].*"  --include="*.vim"  . -R --color -oh
+#grep  -E  "[\"]?Bundle\s+[\"'].*"  --include="*.vim"  . -R --color -oh
+#grep  -E  "[^\"]?Bundle\s+[\"'].*"  --include="*.vim"  . -R --color -oh
+
+echo "=================== used plugins ====================="
+grep  -E  "\"?Bundle\s+[\"'].*"  --include="*.vim"  . -R --color -oh | grep -v "\"Bundle"
+
+# env 是为了使用/bin/echo, 不同版本echo表现不同, sh的内置echo不支持-n
+env echo -e "\n=================== no used plugins ====================="
+grep  -E  "\"Bundle\s+[\"'].*"  --include="*.vim"  . -R --color -oh
 
