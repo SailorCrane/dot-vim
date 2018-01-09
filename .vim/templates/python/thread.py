cedf8968961b80e525167e4597ccae89cb353df3
Crane <215379951@qq.com>
2018-01-09 14:31:20 +0800

    modified:   thread.py
    +    # threading.Timer is a subclass of threading.Thread
    +    t = threading.Timer(1, do_add)
    +    t.start()

--- .vim/templates/python/thread.py
+++ .vim/templates/python/thread.py
@@ -3,8 +3,12 @@
 import threading
 
 def do_add():
-    pass
+    print("do_add")
 
 def main():
     t1 = threading.Thread(target = do_add, args = ())
     t1.start()
+
+    # threading.Timer is a subclass of threading.Thread
+    t = threading.Timer(1, do_add)
+    t.start()
