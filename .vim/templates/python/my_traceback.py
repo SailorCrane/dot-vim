5a014c75233de7015131b7bdd9f51c17f57f35de
Crane <215379951@qq.com>
2018-04-10 12:12:57 +0800

    modified:   .vim/templates/python/my_traceback.py

--- .vim/templates/python/my_traceback.py
+++ .vim/templates/python/my_traceback.py
@@ -1,13 +1,15 @@
 
 import traceback
 
-try:
-    raise ValueError()
-# except Exception as e:
-except ValueError as e:
-    # 1
-    traceback.print_exc()
-    pass
+def err_trace():
+    try:
+        raise ValueError()
+    # except Exception as e:
+    except ValueError as e:
+        # 1
+        traceback.print_exc()
+        pass
 
 # 2
-traceback.print_stack()
+def fun_trace():
+    traceback.print_stack()
