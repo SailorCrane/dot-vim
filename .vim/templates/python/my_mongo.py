68ed93754821a7c74e888af4f3375f4e952caa1d
Crane <215379951@qq.com>
2018-05-08 16:08:51 +0800

    modified:   .vim/templates/python/my_mongo.py

--- .vim/templates/python/my_mongo.py
+++ .vim/templates/python/my_mongo.py
@@ -2,8 +2,11 @@ import pymongo
 
 def mongo():
     client = pymongo.MongoClient('localhost')
+
+    # db + table
     db = client['newtestdb']
     table = db['table']
+
     table.insert({'name' : 'Bob'})
     record = table.find_one( {'name' : 'Bob'} )
     print(record)
