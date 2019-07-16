7d6ce74600a6e31b77c0b70699b0ce4c2db51bed
Crane <215379951@qq.com>
2019-07-16 14:14:14 +0800

    modified:   .vim/templates/=template=.java

--- .vim/templates/=template=.java
+++ .vim/templates/=template=.java
@@ -5,13 +5,18 @@
  * Distributed under terms of the %LICENSE% license.
  */
 
+
+import java.lang.System;
+
+
 public class %FILE%
 {
     public static void main(String[] args) throws Exception {
-		%HERE%
+        System.out.println("start main");
+        %HERE%
     }
 
-	public %FILE%() {
-	}
+    public %FILE%() {
+    }
 }
 
