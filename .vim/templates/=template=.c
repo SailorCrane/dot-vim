ad3021da472ad90161bb1270833f5b7a357a93b9
Crane <215379951@qq.com>
2019-09-18 14:43:07 +0800

    new file:   .vim/templates/=template=.c modified:   .vim/templates/c.templates
    template of c

---
+++ .vim/templates/=template=.c
@@ -0,0 +1,16 @@
+/*
+ * =template=.c
+ * Copyright (C) 2019 crane <crane@gosun>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#include "%FILE%.h"
+#include <stdio.h>
+
+
+int main(int argc, char **argv)
+{
+    printf("main %d", 0);
+    return 0;
+}
