58bb9670248286104d49791724e4116d500dd80f
Crane <myemail@gmail.com>
2020-08-14 17:29:28 +0800

    modified:   .vim/templates/=template=.go

--- .vim/templates/=template=.go
+++ .vim/templates/=template=.go
@@ -7,12 +7,21 @@
 
 package main
 
-
 import (
-    "fmt"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
+func wait_ctrl_c() {
+	signalCh := make(chan os.Signal, 1)
+	signal.Notify(signalCh, os.Interrupt, syscall.SIGTERM)
+	<-signalCh
+}
 
 func main() {
-    fmt.Println("start main")
+	fmt.Println("start main")
+
+	wait_ctrl_c()
 }
